8e63bfb07bd16944a9f4e163719d266803c53972
Theucalyptus <28013186+Theucalyptus@users.noreply.github.com>
2023-09-27 11:05:54 +0200

    Code exo 3

--- src/jeu_devin_exo3.adb
+++ src/jeu_devin_exo3.adb
@@ -4,12 +4,38 @@ with Alea;
 with Jeu_Devin_Exo1;
 with Jeu_Devin_Exo2;
 
--- Auteur : 
---
--- TODO: à compléter...
+-- Auteur : Antoine Lebeault - ENSEEIHT 1A SN - Groupe D
+
 procedure Jeu_Devin_Exo3 is
+
+    Quitter: Boolean;
+    choix_int: Integer;
+
 begin
-	Put_Line ("Jeu_Devin_Exo3 : à changer !");
-	Jeu_Devin_Exo1;
-	Jeu_Devin_Exo2;
+    Quitter := False;
+
+    loop
+        -- Afficher le menu
+        Put("1- L'ordinateur choisit un nombre et vous le devinez");
+        New_Line;
+        Put("2- Vous choisissez un nombre et l'ordinateur le devine");
+        New_Line;
+        Put("0- Quitter le programme");
+
+        -- Demander le choix de l'utilisateur
+        Put("Votre choix : ");
+        Get(choix_int);
+
+        -- Traiter le choix de l'utilisateur
+        case choix_int is
+            when 0 => Put("Au revoir...");
+                      Quitter := True;
+            when 1 => Jeu_Devin_Exo1;
+            when 2 => Jeu_Devin_Exo2;
+            when others =>  Put("Choix incorrect.");
+                            New_Line; 
+        end case;
+    exit when Quitter;
+    end loop;
+   
 end Jeu_Devin_Exo3;
