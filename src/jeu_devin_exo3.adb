74aa36754c2f2391b15b54bbb5acdecca79d4f41
Theucalyptus <28013186+Theucalyptus@users.noreply.github.com>
2023-09-27 11:14:17 +0200

    Update formattage texte exo 3

--- src/jeu_devin_exo3.adb
+++ src/jeu_devin_exo3.adb
@@ -16,23 +16,28 @@ begin
 
     loop
         -- Afficher le menu
+        New_Line;
         Put("1- L'ordinateur choisit un nombre et vous le devinez");
         New_Line;
         Put("2- Vous choisissez un nombre et l'ordinateur le devine");
         New_Line;
         Put("0- Quitter le programme");
+        New_Line;
 
         -- Demander le choix de l'utilisateur
         Put("Votre choix : ");
         Get(choix_int);
 
+
         -- Traiter le choix de l'utilisateur
         case choix_int is
-            when 0 => Put("Au revoir...");
-                      Quitter := True;
+            when 0 =>   New_Line;
+                        Put("Au revoir...");
+                        Quitter := True;
             when 1 => Jeu_Devin_Exo1;
             when 2 => Jeu_Devin_Exo2;
-            when others =>  Put("Choix incorrect.");
+            when others =>  New_Line;
+                            Put("Choix incorrect.");
                             New_Line; 
         end case;
     exit when Quitter;
